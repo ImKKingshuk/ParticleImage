d4cf921683738e805a11a5ddc161e3bb321b914d
ImKKingshuk <126203172+ImKKingshuk@users.noreply.github.com>
2024-02-26 21:55:41 +0530

    updated

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { ParticleImage as ReactParticleImage } from "./react/ParticleImage";
+
+import VueParticleImage from "./vue/VueParticleImage.vue";
+export { VueParticleImage };
+
+export { default as SvelteParticleImage } from "./svelte/SvelteParticleImage.svelte";
